af860075854ec2dcae41b40c675d0754fa133c59
ZDeutch <zander.deutch@menloschool.org>
2025-03-18 13:32:20 -0700

    Completed generate method

--- src/SpellingBee.java
+++ src/SpellingBee.java
@@ -1,13 +1,14 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+// Spelling Bee by Zander Deutch
 
 /**
  * Spelling Bee
- *
+ * <p>
  * This program accepts an input of letters. It prints to an output file
  * all English words that can be generated from those letters.
- *
+ * <p>
  * For example: if the user inputs the letters "doggo" the program will generate:
  * do
  * dog
@@ -18,20 +19,20 @@ import java.util.Scanner;
  * gogo
  * goo
  * good
- *
+ * <p>
  * It utilizes recursion to generate the strings, mergesort to sort them, and
  * binary search to find them in a dictionary.
  *
  * @author Zach Blick, [ADD YOUR NAME HERE]
- *
+ * <p>
  * Written on March 5, 2023 for CS2 @ Menlo School
- *
+ * <p>
  * DO NOT MODIFY MAIN OR ANY OF THE METHOD HEADERS.
  */
 public class SpellingBee {
 
     private String letters;
-    private ArrayList<String> words;
+    public ArrayList<String> words;
     public static final int DICTIONARY_SIZE = 143091;
     public static final String[] DICTIONARY = new String[DICTIONARY_SIZE];
 
@@ -45,12 +46,54 @@ public class SpellingBee {
     //  that will find the substrings recursively.
     public void generate() {
         // YOUR CODE HERE — Call your recursive method!
+        words = generateHelper("", letters);
+
+    }
+
+    public ArrayList<String> generateHelper(String s, String letters) {
+        ArrayList<String> helper = new ArrayList<String>();
+        System.out.println("s: " + s);
+        System.out.println("letters: " + letters);
+        if (letters.isEmpty()) {
+            if(s.isEmpty()) {
+                return helper;
+            } else {
+                helper.add(s);
+                return helper;
+            }
+        }
+        for (int i = 0; i < letters.length(); i++) {
+            ArrayList<String> tempList = generateHelper(s + letters.charAt(i), letters.substring(0, i) + letters.substring(i + 1));
+            // keep track of letters that were copied
+            // Once first o is seen, then store that in array of chars, and don't run recursive call if you see same letter again
+            for (int j = 0; j < tempList.size(); j++) {
+                helper.add(tempList.get(j));
+            }
+        }
+        if(!s.isEmpty()) {
+            helper.add(s);
+        }
+        return helper;
     }
 
     // TODO: Apply mergesort to sort all words. Do this by calling ANOTHER method
     //  that will find the substrings recursively.
     public void sort() {
         // YOUR CODE HERE
+        mergeSort(words, 0, words.size() - 1);
+    }
+
+    public ArrayList<String> mergeSort(ArrayList<String> arr, int low, int high) {
+        if(high - low == 0) {
+            ArrayList<String> sorted =
+            return arr;
+        }
+
+        int mid = (high + low) / 2;
+
+        ArrayList<String> arr1 = mergeSort(arr, low, mid);
+        ArrayList<String> arr2 = mergeSort(arr, mid + 1, high);
+
     }
 
     // Removes duplicates from the sorted list.
@@ -104,7 +147,7 @@ public class SpellingBee {
             return;
         }
         int i = 0;
-        while(s.hasNextLine()) {
+        while (s.hasNextLine()) {
             DICTIONARY[i++] = s.nextLine();
         }
     }
@@ -126,6 +169,7 @@ public class SpellingBee {
         // Generate and print all valid words from those letters.
         SpellingBee sb = new SpellingBee(letters);
         sb.generate();
+        System.out.println(sb.words);
         sb.sort();
         sb.removeDuplicates();
         sb.checkWords();
